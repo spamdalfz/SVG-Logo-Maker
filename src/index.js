70bf398131f8d19ce84007e22dde689e4108aedb
spamdalfz <coreyvasser@gmail.com>
2023-03-31 23:25:32 -0700

    restructured files and folders /n added main function to start the prompt and write the .svg file.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const SVG = require('@svgdotjs/svg.js');
-const shapes = require('./shapes.js');
-const chalk = require('chalk');
+const { circle, triangle, square } = require('../src/js/shapes');
+const svgWidth = 300;
+const svgHeight = 200;
 
 const promptQuestions = () => {
     const questions = [{
@@ -30,3 +31,23 @@ const promptQuestions = () => {
     ];
     return inquirer.prompt(questions);
 };
+async function main() {
+    const answers = await promptQuestions();
+    console.log(answers);
+
+    const svg = SVG(svgWidth, svgHeight);
+
+    const shapeSize = Math.min(svgWidth, svgHeight) / 2;
+
+    const shape = { circle, triangle, square }[answers.shape](shapeSize, answers.shapeColor);
+    shape.center(svgWidth / 2, svgHeight / 2);
+    svg.add(shape);
+
+    const text = SVG('<text>').text(answers.text).fill(answers.textColor).font({ size: shapeSize / 3 }).center(svgWidth / 2, svgHeight / 2);
+    svg.add(text);
+
+    fs.writeFileSync('examples/logo.svg', svg.svg());
+    console.log('Generated logo.svg');
+}
+
+main();
